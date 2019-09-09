a950e4349554ac0db2071ef0384601c7f5362614
Administrador@LAN.GRUPOSSI.COM <Administrador@LAN.GRUPOSSI.COM>
2019-09-09 09:21:07 -0500

    primer commit ember

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    apiURL: 'http://localhost:5000',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
